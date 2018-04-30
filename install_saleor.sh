94f38e101befdde0ffda3b11a8d1f5906c79c44c
Wasdns <952693358@qq.com>
2018-04-30 12:06:01 +0800

    Add script for installing saleor

---
+++ install_saleor.sh
@@ -0,0 +1,30 @@
+set -e
+
+# install python3.6
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+
+# install Node.js
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+# install PostgreSQL
+touch /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-9.6
+
+# install saleor
+sudo apt-get install build-essential python3-dev python3-pip python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+git clone https://github.com/mirumee/saleor.git
+cd saleor/
+# create virtual environment
+pip3 install --upgrade virtualenv
+virtualenv -p python3.6 env
+source env/bin/activate
+pip install -r requirements.txt
+
+
